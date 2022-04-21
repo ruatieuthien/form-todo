e7b779f6848b997d6ebb72cbbe7fe9b05d8aaba3
Tuấn Anh Lê <letuananh06101997@gmail.com>
2022-04-21 20:23:17 +0700

    form

---
+++ javascript.js
@@ -0,0 +1,18 @@
+"use strict";
+var modal = document.getElementsByClassName('sign');
+var btn = document.getElementsByClassName('btn-add');
+var span = document.getElementsByClassName('close')[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+}; // When the user clicks anywhere outside of the modal, close it
+
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
